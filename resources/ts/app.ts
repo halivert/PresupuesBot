dfdb46a035b65967962004d8689532647b8e0445
Halí V <halivelazquez@outlook.com>
2022-11-21 23:35:28 -0600

    feat: update stuff

---
+++ resources/ts/app.ts
@@ -0,0 +1,4 @@
+import { createApp } from "petite-vue";
+import { baseApp } from "@/baseApp";
+
+createApp(baseApp).mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.scss", "resources/ts/app.ts"],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            "@": "/resources/ts",
+        },
+    },
 });
